c33a3889113868921ce251a4b3cf5565b9fff742
Paciência Canda <paciencia@relashe.com>
2024-09-17 14:44:51 +0100

    [S3-2] - base router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,5 @@
+const ErrorPage: React.FC<{}> = () => {
+  return <h1>This page doesn't seem to exist yet!</h1>;
+};
+
+export default ErrorPage;
