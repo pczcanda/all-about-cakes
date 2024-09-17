3761df0041235b4d0dfaf74a3d38d52c7138b634
Paciência Canda <paciencia@relashe.com>
2024-09-17 11:08:24 +0100

    [S0-1] - initial app setup

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="app">
+      <header className="header">
+        <h1>All about cakes</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
