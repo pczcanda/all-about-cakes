6d526a1acdf4b8e54fe23f9cf6ff619f7cc2210b
Paciência Canda <paciencia@relashe.com>
2024-09-17 13:39:37 +0100

    [S2-4] - form component

---
+++ src/components/NewCakeForm/NewCakeForm.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const NewCakeForm: React.FC<{}> = () => {
+  return <Box className="new-cake-form" component="form"></Box>;
+};
+
+export default NewCakeForm;
--- src/types/cakes.ts
+++ src/types/cakes.ts
@@ -1,9 +1,11 @@
-export interface Cake {
-  id: number;
+export interface BaseCake {
   name: string;
   comment: string;
-  imageUrl: string;
+  imageUrl?: string;
   yumFactor: number;
+}
+export interface Cake extends BaseCake {
+  id: number;
 }
 
 export type CakesList = Cake[];
