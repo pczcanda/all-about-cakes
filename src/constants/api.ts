a28ce2d5e42c3f131709f5631493b7093d88bc4a
Paciência Canda <paciencia@relashe.com>
2024-09-17 11:33:44 +0100

    [S1-2] - update endpoints

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,7 @@
-export const ENDPOINT =
+export const API =
   "http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api";
+
+export const ENDPOINTS = {
+  CAKES: `${API}/cakes`,
+  CAKE: (cakeId: string) => `${API}/cakes/${cakeId}`,
+};
