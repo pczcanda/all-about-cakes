4c8a80bb16ebe16d425fe8814ab76bab2595d606
Paciência Canda <paciencia@relashe.com>
2024-09-17 13:06:58 +0100

    [S2-1] - add cake button tests

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("Cakes List", () => {
   test("renders list of cakes", async () => {
@@ -21,5 +22,24 @@ describe("Cakes List", () => {
     const listItems = await screen.findAllByRole("listitem");
 
     expect(listItems).not.toHaveLength(0);
+  });
+
+  test("allows user to enter details for a new cake", () => {
+    render(<App />);
+
+    const newCakeButton = screen.getByText("Add cake");
+
+    expect(newCakeButton).toBeInTheDocument;
+  });
+
+  test("displays dialog for adding new cake after clicking the 'Add cake' button", () => {
+    render(<App />);
+
+    const newCakeButton = screen.getByText("Add cake");
+    userEvent.click(newCakeButton);
+
+    const dialogTitle = screen.getByText("New Cake");
+
+    expect(dialogTitle).toBeInTheDocument();
   });
 });
